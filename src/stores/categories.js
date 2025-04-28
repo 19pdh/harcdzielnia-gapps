0503d00e0b6a487aeee1d6e52ddf4b94c8bb022d
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2025-04-28 15:15:36 +0200

    fix: image path

--- src/stores/categories.js
+++ src/stores/categories.js
@@ -1,10 +1,10 @@
 import { defineStore } from 'pinia'
 
-const categoriesFiles = import.meta.glob('@/assets/categories/*.png', { eager: true })
+const categoriesFiles = import.meta.glob('/public/categories/*.png', { eager: true })
 
 const customCategories = Object.entries(categoriesFiles).map(([path, _]) => {
   const name = path.split('/').pop().split('.')[0]
-  return { name, link: new URL(path, import.meta.url) }
+  return { name, link: path.replace('public/', '') }
 })
 
 const categories = [
